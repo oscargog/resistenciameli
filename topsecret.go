d0fffa9ee83078e736cb0d88ff1e8bf2aba63ce9
Oscar Gomez <oscargomez@MacBook-Pro-de-Oscar.local>
2021-11-18 00:56:03 -0600

    test Post

--- topsecret.go
+++ topsecret.go
@@ -8,8 +8,16 @@ func main() {
    port := os.Getenv("PORT")
    app := gin.New()
    app.GET("/topsecret/",func (ctx *gin.Context){
-    ctx.String(200, "Esto esta funcionando")
-
+    message := ctx.PostForm("este es un mensaje de la resistencia")
+    satelite := ctx.DefaultPostForm("Kenoby","Obi Wan")
+    ctx.JSON(200,gin.H{
+        "status": "posted",
+        "message" : message,
+        "satelite": satelite,
+    })
+    
    })
+
    app.Run(":"+port)
+
 }
