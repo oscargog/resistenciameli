f049075ce2c3ff89b40122bfba8c71d840dba23b
Oscar Gomez <oscargomez@MacBook-Pro-de-Oscar.local>
2021-11-18 12:22:14 -0600

    receive post json

--- topsecret.go
+++ topsecret.go
@@ -4,36 +4,54 @@ import (
     "os"
     "github.com/gin-gonic/gin"
     "net/http"
-    
+    "log"
 )
-
     
-type Body struct {
-    Satellites []struct {
+type satellites struct {
+   Satellites []struct {
         Name     string   `json:"name"`
         Distance float64  `json:"distance"`
         Message  []string `json:"message"`
     } `json:"satellites"`
 }
 
+
 func main() {
    port := os.Getenv("PORT")
    app := gin.New()
 
-   app.POST("/topsecret/",func(context *gin.Context){
+   app.GET("/topsecret_split/:satellite_name", func(c *gin.Context) {
+        satellite_name := c.Param("name")
+        c.String(http.StatusOK, "Hello %s", satellite_name)
+    })
 
-    body:=Body{}
+   app.POST("/topsecret/",func(context *gin.Context){
+    body:=satellites{}
     if err:=context.BindJSON(&body);err!=nil{
          context.AbortWithError(http.StatusBadRequest,err)
          return
       }
-      //fmt.Println(body)
-      message := string(`RESPONSE CODE: 200 \n {"position": { "x": -100.0,"y": 75.5 },"message": "este es un mensaje secreto" }`)
-      
+      message := string(`RESPONSE CODE: 200{"position": { "x": -100.0,"y": 75.5 },"message": "este es un mensaje secreto" }`)
       context.JSON(http.StatusAccepted,&message)
 
    })
-   
    app.Run(":"+port)
 
 }
+
+func satellitesName(w http.ResponseWriter, r *http.Request) {
+
+    keys, ok := r.URL.Query()["key"]
+    
+    if !ok || len(keys[0]) < 1 {
+        log.Println("Url Param 'key' is missing")
+        return
+    }
+
+    // Query()["key"] will return an array of items, 
+    // we only want the single item.
+    key := keys[0]
+
+    log.Println("Url Param 'key' is: " + string(key))
+}
+
